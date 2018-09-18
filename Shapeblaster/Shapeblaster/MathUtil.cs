cf16ba95d378ed8614ec3f831fa5951c57f0a402
Olle Pettersson <olpe00001@utb.vaxjo.se>
2018-09-18 09:26:53 +0200

    Lagt till spelet

---
+++ Shapeblaster/Shapeblaster/MathUtil.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace ShapeBlaster
+{
+    static class MathUtil
+    {
+        public static Vector2 FromPolar(float angle, float magnitude)
+        {
+            return magnitude * new Vector2((float)Math.Cos(angle), (float)Math.Sin(angle));
+        }
+    }
+}
